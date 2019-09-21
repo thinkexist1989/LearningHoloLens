477f70d468818f9c8fa6f3c49162bc0f3b65592c
Yang.LUO <992534544@qq.com>
2019-09-22 00:09:39 +0800

    Add UnityExample

---
+++ UnityExample/Assets/Scripts/ActiveTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveTest : MonoBehaviour
+{
+    public GameObject m_ParentObject;
+    public GameObject m_ChildObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Parent Object's activeInHierarchy:" + m_ParentObject.activeInHierarchy);
+        print("Parent object's activeSelf:" + m_ParentObject.activeSelf);
+        print("Children Object's activeInHierarchy:" + m_ChildObject.activeInHierarchy);
+        print("Children Object's activeSelf:" + m_ChildObject.activeSelf);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ UnityExample/Assets/Scripts/StaticTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject parantObject = GameObject.Find("ParentGameObject");
+        print("At Beginning, Parent Object's static property is: " + parantObject.isStatic);
+        parantObject.isStatic = !parantObject.isStatic;
+        print("Now, Parent Object's static property is: " + parantObject.isStatic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
