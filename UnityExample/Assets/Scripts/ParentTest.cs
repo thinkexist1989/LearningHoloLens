62904aad12c896b069852e0253adca9dd6fa78b3
Yang.LUO <992534544@qq.com>
2019-09-22 21:41:56 +0800

    Add TransformTest and ParentTest to UnityExample

---
+++ UnityExample/Assets/Scripts/ParentTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(string.Format("获取当前游戏对象{0}的父对象{1}的Transform组件：", gameObject, transform.parent.name));
+        print(string.Format("transform.parent.position = {0}", transform.parent.position));
+        print(string.Format("transform.parent.localRotation = {0}", transform.parent.localRotation));
+        print(string.Format("transform.parent.localScale = {0}", transform.parent.localScale));
+
+        GameObject parentGameObject = transform.Find("SubCube").gameObject; //从当前对象向下查找一个叫SubCube的对象
+        print(string.Format("这个游戏对象的{0}的根对象是：{1}", parentGameObject.name, parentGameObject.transform.root.name));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ UnityExample/Assets/Scripts/TransformTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformTest : MonoBehaviour
+{
+    Quaternion rotation = Quaternion.identity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //输出当前场景中this指向的游戏对象Transform组件中的Position, Rotation, Scale的值
+        print(string.Format("当前游戏对象{0}的postion={1}", gameObject, transform.position)); //全局坐标
+        print(string.Format("当前游戏对象{0}的localPosition={1}", gameObject, transform.localPosition)); //相对parent object的坐标
+
+        print(string.Format("当前游戏对象{0}的eulerAngles={1}", gameObject, transform.eulerAngles)); //全局欧拉角
+        print(string.Format("当前游戏对象{0}的localEulerAngles={1}", gameObject, transform.localEulerAngles)); //相对parent object的欧拉角
+
+        print(string.Format("当前游戏对象{0}的localScale={1}", gameObject, transform.localScale)); //相对parent object的scale，全局的叫lossyScale
+
+        rotation.eulerAngles = new Vector3(0, 30, 0); //将this object绕y轴旋转30°
+        print(string.Format("当前游戏对象{0}的rotation.eulerAngles可以被赋值为{1}", gameObject, rotation.eulerAngles));
+        print(string.Format("当前游戏对象{0}的localRotation={1}", gameObject, transform.localRotation));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
