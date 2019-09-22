2d8fdcd6c344d84eebfc65c3c07d70e5f7eaf605
Yang Luo <992534544@qq.com>
2019-09-22 18:18:27 +0800

    Add Scene, Tag, Layer to UnityExample

---
+++ UnityExample/Assets/Scripts/SceneTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("游戏对象" + this.gameObject + "在" + this.gameObject.scene.name + "场景下");
+        print("游戏对象" + this.gameObject + "所在场景在Scenes in build中的序号为：" + this.gameObject.scene.buildIndex);
+        print("游戏对象" + this.gameObject + "所在场景保存的路径为：" + this.gameObject.scene.path);
+        print("游戏对象" + this.gameObject + "共包含父物体" + this.gameObject.scene.rootCount + "个");
+
+        print("游戏对象" + this.gameObject + "工作在场景部分操作修改后不保存返回true。此时值为：" + this.gameObject.scene.isDirty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ UnityExample/Assets/Scripts/TagLayerTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagLayerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject sphere = GameObject.Find("Sphere"); //Find GameObject "Sphere"
+
+        //Layer
+        print("The GameObject \'Cube\' is on Layer " + this.gameObject.layer); //Due to the script is attached to Cube, so this is point to Cube
+        print("The GameObject \'Sphere\' is on Layer " + sphere.layer);
+
+        //Tag
+        print("The tag of GameObject \'Cube\' is " + this.gameObject.tag); //Due to the script is attached to Cube, so this is point to Cube
+        print("The tag of GameObject \'Sphere\' is " + this.gameObject.tag); //Due to the script is attached to Cube, so this is point to Cube
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
